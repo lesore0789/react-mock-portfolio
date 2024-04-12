b580efed5a45bdb8bb78d4bdd0946a4b32ad487e
Leslie Orellana <lesore0789@gmail.com>
2024-04-11 19:37:47 -0700

    contact form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./Components/About/About"
+import Contact from "./Components/Contact/Contact"
 import Hero from "./Components/Hero/Hero"
 import MyWork from "./Components/MyWork/MyWork"
 import Navbar from "./Components/Navbar/Navbar"
@@ -12,6 +13,7 @@ const App = () => {
       <About />
       <Services />
       <MyWork />
+      <Contact />
     </div>
   )
 }
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-unescaped-entities */
+import './Contact.css'
+import patternTheme from '../../assets/theme_pattern.svg'
+import mailIcon from '../../assets/mail_icon.svg'
+import locationIcon from '../../assets/location_icon.svg'
+import callIcon from '../../assets/call_icon.svg'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="contact-title">
+        <h1>Get In Touch</h1>
+        <img src={patternTheme} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>I'm currently available to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact me anytime.</p>
+          <div className="contact-details">
+            <div className="contact-detail"><img src={mailIcon} alt="" /><p>awesomedev@dev.com</p></div>
+            <div className="contact-detail"><img src={callIcon} alt="" /><p>(555)555-5555</p></div>
+            <div className="contact-detail"><img src={locationIcon} alt="" />LA, California</div>
+          </div>
+        </div>
+        <form className='contact-right'>
+          <label htmlFor="">Your Name</label>
+          <input type="text" name='name' placeholder='Enter your name' />
+          <label htmlFor="">Your Email</label>
+          <input type="email" name="email" placeholder='Enter your email' />
+          <label htmlFor="">Write your message here</label>
+          <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+          <button type='submit' className='contact-submit'>Submit Now</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
