ccaebe5cfe9f7e5ae2f5892e7180493b422df661
Leslie Orellana <lesore0789@gmail.com>
2024-04-11 18:50:21 -0700

    services section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./Components/About/About"
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
+import Services from "./Components/Services/Services"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   )
 }
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,30 @@
+import './Services.css'
+import themePattern from '../../assets/theme_pattern.svg'
+import arrowIcon from '../../assets/arrow_icon.svg'
+import servicesData from '../../assets/services_data'
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <div className="services-title">
+        <h1>My Services</h1>
+        <img src={themePattern} alt="" />
+      </div>
+      <div className="services-container">
+        {servicesData.map((service,index)=> {
+          return <div className='services-format' key={index}>
+            <h3>{service.s_no}</h3>
+            <h2>{service.s_name}</h2>
+            <p>{service.s_desc}</p>
+            <div className="services-readmore">
+              <p>Read More</p>
+              <img src={arrowIcon} alt="" />
+            </div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Services
